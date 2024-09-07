be69930f634efc9306dd0740c5041f36d7ab32cd
Vlad P <vlad@pysarenko.com>
2024-09-07 15:11:11 +0200

    hide Contacts link if not logged in

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import style from './Navigation.module.scss';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth';
 
 export default function Navigation() {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (<nav className={style.nav}>
     <ul className={style.nav}>
       <li>
         <NavLink activeClassName={style.active} className={style.link} to="/">Home</NavLink>
       </li>
+      {isLoggedIn && (
       <li>
         <NavLink activeClassName={style.active} className={style.link} to="/contacts">Contacts</NavLink>
       </li>
+      )}
     </ul></nav>
   );
 }
