967a1db44dde98af7baf65cb1ecaf864ce5ead50
DanSport <bogdan.onoyko@gmail.com>
2024-10-27 22:01:56 +0100

    PersistGate added

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'; 
 import App from './components/App/App';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store'; 
 import 'modern-normalize';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
@@ -11,7 +12,10 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/">
       <Provider store={store}>
-        <App />
+        {/* PersistGate обгортає App і дозволяє зберігати стан Redux */}
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </StrictMode>
